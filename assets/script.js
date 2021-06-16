4aa510dd8dd0c4a6a9e3bd82202f6aab603b766c
jamesj995 <mrjam7@gmail.com>
2021-06-15 22:56:04 -0500

    Working today weather, need to add 5-day

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,22 @@
 var srchBtn = document.querySelector("#searchButton");
+var todayEl = document.querySelector("#todayWeather");
 
 //API call to openweather given the lat and long of the entered city. runs displayWeather function with API return object.
-var getWeatherData = function (lat, long) {
+var getWeatherData = function (lat, long, city) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     lat +
     "&lon=" +
     long +
+    "&units=imperial" +
     "&appid=dd0131dd813be694ba34a4c045ffbbfc";
   console.log(apiUrl);
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        displayWeather(data);
+        displayTodayWeather(data, city);
+        displayFiveDay(data);
         console.log(data);
       });
     } else {
@@ -23,7 +26,38 @@ var getWeatherData = function (lat, long) {
 };
 
 //update html and css with weather data.
-var displayWeather = function (fetchResults) {};
+var displayTodayWeather = function (fetchResults, city) {
+    todayEl.textContent = "";
+
+    var displayCity = document.createElement('h2');
+    displayCity.textContent = city + " " + moment().format('(M/D/Y)');
+    todayEl.appendChild(displayCity);
+
+    var todayTemp = fetchResults.current.temp;
+    var displayTodayTemp = document.createElement('h4');
+    displayTodayTemp.textContent = "Temp: " + todayTemp + "F";
+    todayEl.appendChild(displayTodayTemp);
+
+    var todayWind = fetchResults.current.wind_speed;
+    var displayTodayWind = document.createElement('h4');
+    displayTodayWind.textContent = "Wind Speed: " + todayWind + " MPH";
+    todayEl.appendChild(displayTodayWind);
+
+    var todayHum = fetchResults.current.humidity;
+    var displayTodayHum = document.createElement('h4');
+    displayTodayHum.textContent = "Humidity: " + todayHum + "%";
+    todayEl.appendChild(displayTodayHum);
+
+    var todayUVI = fetchResults.current.uvi;
+    var displayTodayUVI = document.createElement('h4');
+    displayTodayUVI.textContent = "UV Index: " + todayUVI;
+    todayEl.appendChild(displayTodayUVI);
+
+};
+
+var displayFiveDay = function (fetchResults) {
+    
+}
 
 //google maps api geocoder to get latitude and longitude for the given city on search button click. runs the getWeatherData function with the generated lat and long.
 $("#searchButton").click(function () {
@@ -33,9 +67,10 @@ $("#searchButton").click(function () {
     if (status == google.maps.GeocoderStatus.OK) {
       var lat = results[0].geometry.location.lat();
       var long = results[0].geometry.location.lng();
+      var city = results[0].formatted_address;
       lat = Math.round(lat);
       long = Math.round(long);
-      getWeatherData(lat, long);
+      getWeatherData(lat, long, city);
     } else {
       alert("Something is wrong " + status);
     }
